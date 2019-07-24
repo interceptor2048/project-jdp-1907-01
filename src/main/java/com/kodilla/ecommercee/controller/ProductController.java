7b1814250c585f4b1de914462d73b9a3b5f63bd7
Marcel Sztur <sensiss99@gmail.com>
2019-07-24 14:43:54 +0200

    Refactoring after Added ProductController, task:JDP190701-1

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.dto.ProductDto;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,29 +9,28 @@ import java.util.List;
 @RequestMapping("product")
 public class ProductController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "getAllProducts")
+    @GetMapping(value = "getAllProducts")
     public List<ProductDto> getAllProduct() {
         return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getProductById")
+    @GetMapping(value = "getProductById")
     public ProductDto getProductById(@RequestParam int id) {
         return new ProductDto();
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createProduct")
+    @PostMapping(value = "createProduct")
     public ProductDto createProduct(@RequestBody ProductDto productDto) {
         return productDto;
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
+    @PutMapping(value = "updateProduct")
     public ProductDto updateProduct(@RequestBody ProductDto productDto) {
         return productDto;
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
+    @DeleteMapping(value = "deleteProduct")
     public void deleteProduct(@RequestParam int id) {
         //Execute productDao interface to delete object from database
     }
-
 }
--- src/main/java/com/kodilla/ecommercee/dto/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/ProductDto.java
@@ -4,21 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ProductDto {
-
     private  int id;
-
     private String name;
-
     private String description;
-
     private double price;
-
     private int groupId;
-
-
 }
