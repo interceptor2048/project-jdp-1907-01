b4becf0ea558169671a71e25da61b43b00f601cb
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-07-24 19:39:47 +0200

    Implementation of CartController, task: JDP190701-3

---
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -0,0 +1,38 @@
+package com.kodilla.ecommercee.controller;
+
+import com.kodilla.ecommercee.dto.CartDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/user")
+public class CartController {
+
+    @PostMapping(value = "{userId}/cart")
+    public  void createCart(@PathVariable Long userId) {
+    }
+
+    @GetMapping(value = "{userId}/cart")
+    public CartDto getCart(@PathVariable Long userId) {
+        return new CartDto(1L,userId,new ArrayList<>());
+    }
+
+    @PostMapping(value = "{userId}/cart/{itemId}")
+    public  void addItemToCard(@PathVariable Long userId, @PathVariable Long itemId,  @RequestParam Long itemQuantity) {
+    }
+
+    @PutMapping(value = "{userId}/cart/{itemId}")
+    public CartDto updateCartItem(@PathVariable Long userId, @PathVariable Long itemId, @RequestParam Long itemQuantity) {
+        return new CartDto(1L,userId,new ArrayList<>());
+    }
+
+    @DeleteMapping(value = "{userId}/cart/{itemId}")
+    public void removeItemFromCard(@PathVariable Long userId,@PathVariable  @RequestParam Long itemId) {
+    }
+
+
+
+
+}
