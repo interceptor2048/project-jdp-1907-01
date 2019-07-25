ab88a8c7610e22fa782bc5d5f77f68988496c883
dazz4 <kaminski.darek@icloud.com>
2019-07-25 18:42:41 +0100

    Optimized imports

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,12 +1,9 @@
 package com.kodilla.ecommercee.domain;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @AllArgsConstructor
