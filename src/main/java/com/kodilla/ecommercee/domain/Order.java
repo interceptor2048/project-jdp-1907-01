bbbde50416292592bfb0f6cbd139e57e52c5861f
MAcewicz <acewicz.m@gmail.com>
2019-07-26 21:04:53 +0100

    Implementation of Order entity, OrderMapper and OrderRepository

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -4,22 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Entity(name = "orders")
+@Entity
 public class Order {
 
     @Id
     @GeneratedValue
     private long id;
     private LocalDate date;
+    private boolean isCompleted;
+    @OneToMany(targetEntity = Product.class, mappedBy="order", fetch=FetchType.LAZY)
     private List<Product> productList;
     private long userId;
+
 }
--- src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Component;
 public class OrderMapper {
 
     public Order mapToOrder(OrderDto orderDto) {
-        return new Order(orderDto.getId(), orderDto.getDate(), orderDto.getProductList(), orderDto.getUserId());
+        return new Order(orderDto.getId(), orderDto.getDate(), orderDto.isCompleted(), orderDto.getProductList(), orderDto.getUserId());
     }
 
     public OrderDto mapToOrderDto(Order order) {
-        return new OrderDto(order.getId(), order.getDate(), order.getProductList(), order.getUserId());
+        return new OrderDto(order.getId(), order.getDate(), order.isCompleted(), order.getProductList(), order.getUserId());
     }
 
 }
