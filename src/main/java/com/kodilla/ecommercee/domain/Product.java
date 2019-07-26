7034dafb1eeceb971de9f8d3dfca4877be0c18f6
MAcewicz <acewicz.m@gmail.com>
2019-07-26 21:06:45 +0100

    Implementation of Order entity, OrderMapper and OrderRepository

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,4 +1,24 @@
 package com.kodilla.ecommercee.domain;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String productName;
+    private BigDecimal price;
+    private long groupId;
+    @ManyToOne
+    @JoinColumn(name = "orderId")
+    private Order order;
 }
--- src/main/java/com/kodilla/ecommercee/dto/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/OrderDto.java
@@ -4,17 +4,20 @@ package com.kodilla.ecommercee.dto;
 import com.kodilla.ecommercee.domain.Product;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.LinkedHashMap;
+import java.util.List;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class OrderDto {
 
     private Long id;
     private LocalDate date;
-    private LinkedHashMap<Product, Long> productMap;
+    private boolean isCompleted;
+    private List<Product> productList;
     private Long userId;
 
 
