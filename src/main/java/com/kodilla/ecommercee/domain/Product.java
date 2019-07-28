3eeead5b175f12ea939cffa06f56ebfe1479361a
mrasinski <ariel_6@wp.pl>
2019-07-28 10:33:38 +0200

    add constructor for Product

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -14,12 +14,22 @@ public class Product {
     @Id
     @GeneratedValue
     private Long id;
-    private String productName;
+    private String name;
+    private String description;
     private BigDecimal price;
-    @OneToOne
+
+    @ManyToOne
     @JoinColumn(name = "groupId")
     private Group group;
+
     /*@ManyToOne
     @JoinColumn(name = "orderId")
     private Order order;*/
+
+    public Product(String name, String description, BigDecimal price, Group group) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.group = group;
+    }
 }
