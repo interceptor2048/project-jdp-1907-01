3a76985bce9d116941bca324445310251c1ee593
Anna Leja <anna.leja16@gmail.com>
2019-07-29 22:21:59 +0200

    add Cart entity

--- src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -1,23 +1,20 @@
 package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.domain.Cart;
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 import java.util.Optional;
 
 public interface CartRepository extends CrudRepository<Cart, Long> {
 
+    @Override
+    Optional<Cart> findById(Long cartId);
 
     @Override
-    Cart findByUserId(Long userId);
-
-    @Override
-    Optional<Cart> findById(Long productId);
+    List<Cart> findAll();
 
     @Override
     Cart save(Cart cart);
 
     @Override
-    void deleteById(Long productId);
-
-    @Override
-    Cart findByCartId(Long cartId);
+    void deleteById(Long cartId);
 }
