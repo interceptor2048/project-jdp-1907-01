1b54d12d9e6c01f114e5178ee191c92e5e1c06ed
Marcel Sztur <sensiss99@gmail.com>
2019-07-29 20:10:32 +0200

    Real implementation of Order Controller methods, task: task-JDP190701-24

--- src/test/java/com/kodilla/ecommercee/domain/OrderEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/OrderEntityTestSuite.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
