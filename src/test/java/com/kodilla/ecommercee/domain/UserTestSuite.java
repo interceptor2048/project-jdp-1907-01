f8cf82cc6c76a30dd0fb62dd94ade8023b8726b4
Andrzej Jaromin <ajaromin@silenteight.com>
2019-07-29 21:55:13 +0200

    Modify test to check CircleCI issue

--- src/test/java/com/kodilla/ecommercee/domain/UserTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/UserTestSuite.java
@@ -39,56 +39,56 @@ public class UserTestSuite {
         long numberOfRecords = userRepository.count();
         //Then
         System.out.println(numberOfRecords);
-//        assertEquals(1,numberOfRecords);
+        assertEquals(1,numberOfRecords);
         //Clean Up
         userRepository.deleteById(user.getId());
     }
 
-    @Test
-    public void shouldDeleteFromDatabase() {
-        //Given
-        User user = createContent();
-        userRepository.save(user);
-        LOGGER.info("Records in table: " + userRepository.count());
-        userRepository.deleteById(user.getId());
-        LOGGER.info("Records in table: " + userRepository.count());
-        //When
-        long numberOfRecords = userRepository.count();
-        //Then
-        System.out.println(numberOfRecords);
-//        assertEquals(0,numberOfRecords);
-    }
-
-    @Test
-    public void shouldFindUserById() {
-        //Given
-        User user = createContent();
-        userRepository.save(user);
-        //When
-        Optional resultUser = userRepository.findById(user.getId());
-        //Then
-        System.out.println(resultUser);
-//        assertTrue(Optional.ofNullable(resultUser).isPresent());
-        //Clean Up
-        userRepository.deleteById(user.getId());
-    }
-
-    @Test
-    public void shouldUpdateUser() {
-        //Given
-        User user = createContent();
-        userRepository.save(user);
-        User updateUser = new User(user.getId(),"UpdateJessie","updateBusy",567L);
-        userRepository.save(updateUser);
-        //When
-        List<User> resultListOfUsers = userRepository.findAll();
-        User resultUser = resultListOfUsers.get(0);
-        //Then
-        System.out.println(resultListOfUsers);
-//        assertEquals(1,resultListOfUsers.size());
-//        assertEquals("UpdateJessie",resultUser.getUsername());
-//        assertEquals("updateBusy",resultUser.getStatus());
-        //Clean Up
-        userRepository.deleteById(user.getId());
-    }
+//    @Test
+//    public void shouldDeleteFromDatabase() {
+//        //Given
+//        User user = createContent();
+//        userRepository.save(user);
+//        LOGGER.info("Records in table: " + userRepository.count());
+//        userRepository.deleteById(user.getId());
+//        LOGGER.info("Records in table: " + userRepository.count());
+//        //When
+//        long numberOfRecords = userRepository.count();
+//        //Then
+//        System.out.println(numberOfRecords);
+////        assertEquals(0,numberOfRecords);
+//    }
+//
+//    @Test
+//    public void shouldFindUserById() {
+//        //Given
+//        User user = createContent();
+//        userRepository.save(user);
+//        //When
+//        Optional resultUser = userRepository.findById(user.getId());
+//        //Then
+//        System.out.println(resultUser);
+////        assertTrue(Optional.ofNullable(resultUser).isPresent());
+//        //Clean Up
+//        userRepository.deleteById(user.getId());
+//    }
+//
+//    @Test
+//    public void shouldUpdateUser() {
+//        //Given
+//        User user = createContent();
+//        userRepository.save(user);
+//        User updateUser = new User(user.getId(),"UpdateJessie","updateBusy",567L);
+//        userRepository.save(updateUser);
+//        //When
+//        List<User> resultListOfUsers = userRepository.findAll();
+//        User resultUser = resultListOfUsers.get(0);
+//        //Then
+//        System.out.println(resultListOfUsers);
+////        assertEquals(1,resultListOfUsers.size());
+////        assertEquals("UpdateJessie",resultUser.getUsername());
+////        assertEquals("updateBusy",resultUser.getStatus());
+//        //Clean Up
+//        userRepository.deleteById(user.getId());
+//    }
 }
