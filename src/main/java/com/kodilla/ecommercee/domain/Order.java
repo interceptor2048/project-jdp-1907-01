b0d99c6c2afcfc3a9002a5ea7f2ce695bbb8a6f0
Marcel Sztur <sensiss99@gmail.com>
2019-07-31 18:12:44 +0200

    Refactorin

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -3,10 +3,12 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,13 +22,15 @@ public class Order {
     private Long id;
     private LocalDate date;
     private boolean isCompleted;
-//    @OneToMany(targetEntity = Product.class, mappedBy="order", fetch=FetchType.LAZY)
-//    private List<Product> productList;
-    private Long userId;
+    @OneToMany(targetEntity = Product.class, mappedBy="order", fetch=FetchType.LAZY)
+    private List<Product> productList;
+    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    @Setter
+    private User user;
 
-    public Order(LocalDate date, boolean isCompleted, Long userId) {
+    public Order(LocalDate date, boolean isCompleted) {
         this.date = date;
         this.isCompleted = isCompleted;
-        this.userId = userId;
     }
 }
