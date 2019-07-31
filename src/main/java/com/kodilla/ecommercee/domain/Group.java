de94979c2c1fcfac7e2079123e394d4de4b56735
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-07-31 20:04:07 +0200

    CleanUp Group class

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -7,21 +7,28 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Entity
-
 @Table(name = "PRODUCT_GROUPS")
 public class Group {
-
     @Id
     @GeneratedValue
     private Long id;
     private String name;
 
+    @OneToMany(mappedBy="group")
+    private List<Product> products;
+
+    public Group(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
     public Group(String name) {
         this.name = name;
     }
