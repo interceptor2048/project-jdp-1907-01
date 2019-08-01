ceb8dd01024902c156a6417ec8ecfd85c1fcdd03
Marcel Sztur <sensiss99@gmail.com>
2019-08-01 11:37:17 +0200

    Refactoring

--- src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -2,12 +2,8 @@ package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-<<<<<<< 1b54d12d9e6c01f114e5178ee191c92e5e1c06ed
 import org.springframework.stereotype.Repository;
-=======
 import org.springframework.data.repository.query.Param;
-
->>>>>>> Refactoring, added relations beetwen user and order , add new method to user reposotory,add unit test for this new function
 import java.util.List;
 import java.util.Optional;
 
