4d83a563de802d9fe642a8fe0a0a8fcd4b17d4be
Marcel Sztur <sensiss99@gmail.com>
2019-08-04 17:00:16 +0200

    WIP on testSaveAndGetOrder() test case

--- src/test/java/com/kodilla/ecommercee/service/OrderServiceTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/service/OrderServiceTestSuite.java
@@ -1,4 +1,5 @@
 package com.kodilla.ecommercee.service;
+
 import com.kodilla.ecommercee.controller.exceptions.OrderNotFoundException;
 import com.kodilla.ecommercee.domain.Order;
 import com.kodilla.ecommercee.domain.User;
@@ -7,8 +8,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import java.time.LocalDate;
 import java.util.List;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -21,26 +24,27 @@ public class OrderServiceTestSuite {
     @Test
     public void testSaveAndGetOrder() throws OrderNotFoundException {
         //Given
-        Order order = new Order(LocalDate.now(), true, new User());
+        Order order = new Order(13L,LocalDate.now(), true, new User());
         //When
         orderService.saveOrder(order);
-        Order tempOrder = orderService.getOrder(order.getId()).orElseThrow(OrderNotFoundException::new);
+        Order tempOrder = orderService.getOrder(order.getId());
         List<Order> tempOrders = orderService.getOrders();
         //Then
         assertNotNull(tempOrder);
         assertNotNull(tempOrders);
         assertEquals(LocalDate.now(), tempOrder.getDate());
-        assertEquals(1, tempOrders.size());
     }
 
     @Test
-    public void testDeleteUser() {
+    public void testDeleteUser()  {
         //Given
         Order order = new Order(LocalDate.now(), true, new User());
         //When
         orderService.saveOrder(order);
+        List<Order> ordersAfterAdd = orderService.getOrders();
         orderService.deleteOrder(order.getId());
+        List<Order> ordersAfterRemove = orderService.getOrders();
         //Then
-        assertFalse(orderService.getOrder(order.getId()).isPresent());
+        assertTrue(ordersAfterAdd.size() - ordersAfterRemove.size() == 1);
     }
 }
