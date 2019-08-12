ab9b68e7daa9bc34e3c46dce7103315ecede8131
MAcewicz <acewicz.m@gmail.com>
2019-08-12 20:27:52 +0100

    poprawki

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -4,8 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.NamedQuery;
 
 @NamedQuery(
         name = "User.returnUserById",
@@ -30,7 +37,6 @@ public class User {
     @JoinColumn(name = "cartId")
     private Cart cart;
 
-
     public User(String username, String status, Long userKey, String phoneNumber) {
         this.username = username;
         this.status = status;
@@ -38,14 +44,11 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
-    public User(String username, String status, Long userKey,String phoneNumber, Cart cart) {
-
+    public User(String username, String status, Long userKey, String phoneNumber, Cart cart) {
         this.username = username;
         this.status = status;
         this.userKey = userKey;
         this.phoneNumber = phoneNumber;
         this.cart = cart;
     }
-
-
 }
--- src/test/java/com/kodilla/ecommercee/domain/CartTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/CartTestSuite.java
@@ -1,5 +1,4 @@
 package com.kodilla.ecommercee.domain;
-
 import com.kodilla.ecommercee.repository.CartRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -21,6 +21,7 @@ public class CartTestSuite {
     private Cart createTestCart() {
         return new Cart();
     }
+
 
     @Test
     public void testSaveCart() {
